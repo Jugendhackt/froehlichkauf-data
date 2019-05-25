c365c5b770e164348963201cc310ccb22238fa01
XeEtDe <xedestuff@gmail.com>
2019-05-25 12:01:09 +0200

    Anfang

---
+++ Datenalgorithmus.py
@@ -0,0 +1,24 @@
+class Produkt(Name, Inhaltsstoffe, Nährwerte, Verpackung, Herkunftsort, Herstellungsbedingungen):
+    self.Inhaltsstoffe = Inhaltsstoffe #Liste
+    self.Nährwerte = Nährwerte #Liste, pro 100 Gramm, [Brennwert in kj, Fett g, Kohlenhydrate g, Zucker g, Eiweiß g, Salz g]
+    self.Verpackung = Verpackung # 0 = Plastik, 1 = Papier
+    self.Herkunftsort = Herkunftsort #nach Entfernung
+    self.Herstellungsbedingungen = Herstellungsbedingungen #Info über Firma insgesamt, 0 (schlecht) bis 5 (gut)
+
+#Bio und Fairtrade
+
+Land_des_Einkäufers = "DE"
+
+Produkt("nippon", [], [2229, 30.1, 58.9, 40.2, 5.6, 0.22], 0, "DE", 0)
+
+Gefährliche_Inhaltsstoffe: {}
+
+def Gesundheit(Produkt):
+
+def Umwelt(Produkt):
+    #Herkunftsort, selbes Land ist gut
+    if Land_des_Einkäufers == Produkt.Herkunftsort:
+        
+
+def Ethik(Produkt):
+    return Produkt.Herstellungsbedingungen
